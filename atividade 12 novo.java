a5b4afff34e69ed7a50bb8e40b705604f1f64a84
israel203010 <111544955+israel203010@users.noreply.github.com>
2022-10-07 17:33:23 -0300

    Add files via upload

---
+++ atividade 12 novo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner inicio = new Scanner (System.in);
+    int a, b;
+    System.out.println("por favor digite o valor de a");
+    a = inicio.nextInt();
+    System.out.println ();
+
+    for (b=1;  b<=a; b++){
+      if ((b%2) ==1){
+      System.out.println(b);
+    
+  }
+}
+  }
+}
